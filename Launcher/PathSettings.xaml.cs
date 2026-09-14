ebd7c81c2a6416d318f6a68972739d81f8dd0f43
Jake Boraman <jake.boraman@gmail.com>
2026-09-14 15:20:19 +0100

    Added FSB fix checkbox for Reach profiles

--- Launcher/PathSettings.xaml.cs
+++ Launcher/PathSettings.xaml.cs
@@ -120,10 +120,17 @@ namespace ToolkitLauncher
         private void save_button_Click(object sender, RoutedEventArgs e)
         {
             ToolkitProfiles.Save();
+
             if (gen_type.SelectedIndex == 3 && !(bool)is_mcc.IsChecked)
             {
                 ReachToolPatcher.PatchLightmapColorAssert(lm_color_fix.IsChecked ?? false, tool_path.Text, tool_fast_path.Text);
             }
+
+            if (gen_type.SelectedIndex == 3 && !(bool)is_mcc.IsChecked)
+            {
+                // Do stuff
+            }
+
             this.Close();
         }
 
@@ -286,6 +293,7 @@ namespace ToolkitLauncher
             expert_mode.IsChecked = false;
             batch.IsChecked = false;
             lm_color_fix.IsChecked = false;
+            fsb_corruption_fix.IsChecked = false;
             h2codez_update_groupbox.Visibility = Visibility.Collapsed;
             if (profile_select != null && profile_select.SelectedItem != null && ToolkitProfiles.SettingsList.Count > profile_index && profile_index >= 0)
             {
@@ -306,6 +314,7 @@ namespace ToolkitLauncher
                 expert_mode.IsChecked = ToolkitProfiles.SettingsList[profile_index].ExpertMode;
                 batch.IsChecked = ToolkitProfiles.SettingsList[profile_index].Batch;
                 lm_color_fix.IsChecked = ToolkitProfiles.SettingsList[profile_index].ReachColorAssertFix;
+                fsb_corruption_fix.IsChecked = ToolkitProfiles.SettingsList[profile_index].ReachFSBImportFix;
 
                 h2codez_update_groupbox.Visibility = ToolkitProfiles.SettingsList[profile_index].IsH2Codez() ?
                     Visibility.Visible : Visibility.Collapsed;
@@ -360,6 +369,7 @@ namespace ToolkitLauncher
                 ExpertMode = (bool)expert_mode.IsChecked,
                 Batch = (bool)batch.IsChecked,
                 ReachColorAssertFix = (bool)lm_color_fix.IsChecked,
+                ReachFSBImportFix = (bool)fsb_corruption_fix.IsChecked
             };
 
             // get new and old base directory
